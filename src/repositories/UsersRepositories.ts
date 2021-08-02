46400d18d1236fdf8ce46fcd69c0524b62d24d64
Tarcisio Rodrigues <tarcisiorodrigues454@gmail.com>
2021-08-02 11:25:06 -0300

    structure User

---
+++ src/repositories/UsersRepositories.ts
@@ -0,0 +1,6 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from '../enity/User';
+@EntityRepository(User)
+class UsersRepositories extends Repository<User> {}
+
+export { UsersRepositories };
